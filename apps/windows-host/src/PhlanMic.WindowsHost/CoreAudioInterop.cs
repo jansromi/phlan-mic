b1c1d2eb3caa47bb28f5a19e132506f7b2559e68
jansromi <rrobertjansson@gmail.com>
2026-04-04 22:47:25 +0300

    fix: update GUID for IMMDeviceCollection interface

--- apps/windows-host/src/PhlanMic.WindowsHost/CoreAudioInterop.cs
+++ apps/windows-host/src/PhlanMic.WindowsHost/CoreAudioInterop.cs
@@ -118,7 +118,7 @@ internal static class CoreAudioInterop
     }
 
     [ComImport]
-    [Guid("0BD7A1BE-7A1A-44DB-8397-C0A3032BDB79")]
+    [Guid("0BD7A1BE-7A1A-44DB-8397-CC5392387B5E")]
     [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
     internal interface IMMDeviceCollection
     {
